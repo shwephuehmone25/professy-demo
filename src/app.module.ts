cdebe11f6a8ba4efdcf64e70962399772caf528a
shwephuehmone25 <shwephue7889@gmail.com>
2025-09-09 10:39:11 +0630

    feat(scraper): Configured imports and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    ScraperModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // ConfigService
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 3000;
+
+  await app.listen(port);
+
+  //console.log(`Application is running on http://localhost:${port}`);
+  //console.log(`Google API Key loaded: ${!!configService.get('GOOGLE_API_KEY')}`);
+  //console.log(`MongoDB URI loaded: ${!!configService.get('MONGO_URI')}\n`);
 }
+
 bootstrap();
